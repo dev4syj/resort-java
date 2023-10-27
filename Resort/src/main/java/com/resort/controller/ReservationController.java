4733d30e850dcda3debbf8ef186bb5cc0cb153b0
dev4syj <129806812+dev4syj@users.noreply.github.com>
2023-10-27 17:40:52 +0900

    update: 예약 중복 여부 확인

--- Resort/src/main/java/com/resort/controller/ReservationController.java
+++ Resort/src/main/java/com/resort/controller/ReservationController.java
@@ -1,23 +1,53 @@
 package com.resort.controller;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.resort.domain.Reservation;
+import com.resort.repository.ReservationRepository;
+import com.resort.service.ReservationService;
 
 @Controller
 public class ReservationController {
 
-//	@Autowired
-//	private ReservationService reservationService;
-	
+	@Autowired
+	private ReservationRepository reservationRepository;
+
+	@Autowired
+	private ReservationService reservationService;
+
 	@GetMapping("/status")
 	public String status() {
 		return "status";
 	}
 
-//	@GetMapping("/status")
-//	public ResponseEntity<List<Reservation>> getReservations() {
-//		List<Reservation> reservations = reservationService.getAllReservations();
-//		return new ResponseEntity<>(reservations, HttpStatus.OK);
-//	}
+	@GetMapping("/reservationList")
+	public String reservationList(Model model) {
+		List<Reservation> reservationList = reservationRepository.findAll();
+		model.addAttribute("reservationList", reservationList);
+		return "reservation_list";
+	}
+
+	@PostMapping("/check-duplicate-reservation")
+	public ResponseEntity<String> checkDuplicateReservation(@RequestBody Reservation reservation) {
+		Date checkInDate = reservation.getCheckInDate();
+		Date checkOutDate = reservation.getCheckOutDate();
+		int roomId = reservation.getRoomId();
+
+		if (reservationService.isDuplicateReservation(roomId, checkInDate, checkOutDate)) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Reservation is duplicated.");
+		} else {
+			return ResponseEntity.ok("Reservation is available.");
+		}
+	}
 
 }
--- Resort/src/main/java/com/resort/service/ReservationService.java
+++ Resort/src/main/java/com/resort/service/ReservationService.java
@@ -1,12 +1,11 @@
 package com.resort.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import com.resort.domain.Reservation;
 import com.resort.repository.ReservationRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,14 +13,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class ReservationService {
-	
+
 	@Autowired
 	private final ReservationRepository reservationRepository;
 
-	@GetMapping("/status")
-	public List<Reservation> getAllReservations() {
-		List<Reservation> reservations = reservationRepository.findAll();
-		return reservations;
+	public List<Integer> findAvailableRoomIds(Date checkInDate, Date checkOutDate) {
+		List<Integer> reservedRoomIds = reservationRepository.findReservedRoomIds(checkInDate, checkOutDate);
+		List<Integer> availableRoomIds = reservationRepository.findAvailableRoomIds(reservedRoomIds);
+		return availableRoomIds;
+	}
+
+	public boolean isDuplicateReservation(int roomId, Date checkInDate, Date checkOutDate) {
+		// 중복 예약 확인
+		return reservationRepository.existsByRoomIdAndCheckInDateLessThanEqualAndCheckOutDateGreaterThanEqual(roomId, checkInDate, checkOutDate);
 	}
 
 }
