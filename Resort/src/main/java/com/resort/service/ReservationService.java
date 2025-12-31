8e6c3c6da541f067a73899e2e15b4818aaa9f6b5
dev4syj <user@DESKTOP-CHGDDGC>
2025-12-31 13:52:05 +0900

    Feat: 관리자 예약 관리 기능 추가
    - 예약 상세 조회, 수정, 삭제 API 구현
    - 캘린더 이벤트 클릭 시 예약 상세 모달 표시
    - 예약 수정 페이지 추가

--- Resort/src/main/java/com/resort/service/ReservationService.java
+++ Resort/src/main/java/com/resort/service/ReservationService.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.resort.domain.Reservation;
 import com.resort.domain.ResortUser;
+import com.resort.domain.Room;
 import com.resort.repository.ReservationRepository;
+import com.resort.repository.RoomRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +24,9 @@ public class ReservationService {
 
 	@Autowired
 	private final ReservationRepository reservationRepository;
+
+	@Autowired
+	private final RoomRepository roomRepository;
 
 	public void newReservation(Reservation reservation, ResortUser user) {
 		reservation.setReservationDate(LocalDate.now());
@@ -46,6 +52,7 @@ public class ReservationService {
 		for (Reservation reservation : reservationList) {
 			Map<String, Object> events = new HashMap<>();
 
+			events.put("id", reservation.getReservationId());
 			events.put("start", reservation.getCheckInDate());
 			events.put("title", reservation.getRoomType());
 			events.put("end", reservation.getCheckOutDate());
@@ -63,6 +70,33 @@ public class ReservationService {
 		}
 
 		return eventList;
+	}
+
+	public Optional<Reservation> getReservation(Long id) {
+		return reservationRepository.findById(id);
+	}
+
+	public void deleteReservation(Long id) {
+		reservationRepository.deleteById(id);
+	}
+
+	public void updateReservation(Long id, String checkInDate, String checkOutDate, int adult, int children, Long roomId) {
+		Optional<Reservation> optionalReservation = reservationRepository.findById(id);
+		if (optionalReservation.isPresent()) {
+			Reservation reservation = optionalReservation.get();
+			reservation.setCheckInDate(checkInDate);
+			reservation.setCheckOutDate(checkOutDate);
+			reservation.setAdult(adult);
+			reservation.setChildren(children);
+
+			Optional<Room> room = roomRepository.findById(roomId);
+            room.ifPresent(reservation::setRoomId);
+			reservationRepository.save(reservation);
+		}
+	}
+
+	public List<Room> getAllRooms() {
+		return roomRepository.findAll();
 	}
 
 }
